13bba04fbb4f91deb02fe310eee8a58fe6c5a065
sanjaykumar-cse <111355256+sanjaykumar-cse@users.noreply.github.com>
2022-10-25 19:27:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+class OddNumbers{
+    static main(_string_){
+       for (int  = 0; number < 100; number++){
+           if (number % 2 != 0){
+               system.out.printin(number);
+           };
+       };
+   };
+};
+
